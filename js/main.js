9faab47ffd8e3c9be1c0afb574df57728ca7c89c
Nicolas <nicolas_jofre@outlook.com.ar>
2023-05-03 23:55:43 -0300

    Slider en main

--- js/main.js
+++ js/main.js
@@ -1,18 +1,41 @@
-let slideIndex = 0;
 var prevScrollpos = window.pageYOffset;
-showSlides();
 
-function showSlides() {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 8000); // Cambai el tiempo de iamgen en 8 seg
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
 }
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+function showSlides(n) {
+    let i;
+    if (document.querySelector(".carousel")) {
+        let slideshowPage = document.querySelector(".carousel");
+        if (slideshowPage.classList.contains("carousel")) {
+            let slides = slideshowPage.querySelectorAll(".mySlides");
+            if (n > slides.length) {
+            slideIndex = 1
+            };
+            if (n < 1) {
+            slideIndex = slides.length
+            };
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        };
+        slides[slideIndex-1].style.display = "block";
+        }
+    }
+}
+
+
+setInterval(function() {
+  plusSlides(1)
+}, 5000);
 
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
