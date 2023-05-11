d216a5ea1b24b3a0287c339e0b9783aa3726c318
Nicolas <nicolas_jofre@outlook.com.ar>
2023-05-11 18:31:45 -0300

    Cambio de nav.bar side

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 var prevScrollpos = window.pageYOffset;
-
-
 let slideIndex = 1;
 showSlides(slideIndex);
+let menu = document.querySelector('#menu');
+let menu_bar = document.querySelector('#btn-toggle');
+const cerrar = document.querySelector('#close-btn');
 
 // Next/previous controls
 function plusSlides(n) {
@@ -46,3 +47,11 @@ window.onscroll = function() {
   }
   prevScrollpos = currentScrollPos;
 }
+
+menu_bar.addEventListener('click',function(){
+    menu.classList.toggle('btn-togler-event')
+});
+
+cerrar.addEventListener('click', () => {
+    menu.classList.remove('btn-togler-event');
+})
