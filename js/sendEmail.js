c420d1b199023640e48901a99afc4c806b0ce37e
Nicolas <nicolas_jofre@outlook.com.ar>
2023-07-19 17:17:19 +0200

    Arreglo de link de form y cambio de mail

--- js/sendEmail.js
+++
@@ -1,9 +0,0 @@
-const form = document.getElementById('form');
-const sendMail = document.getElementById('emailA');
-
-function handleSendEmail (event) {
-    event.preventDefault();
-    
-}
-
-form.addEventListener('submit', handleSendEmail);
